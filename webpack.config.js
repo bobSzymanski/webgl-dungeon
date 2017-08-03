9a382e7513a0b9190823cbbe19988d6c4c34f7e3
Bob Szymanski <Bob.Szymanski@concur.com>
2017-08-03 15:43:46 -0700

    figured out how this works again

--- webpack.config.js
+++ webpack.config.js
@@ -1,15 +1,15 @@
 module.exports = {
-    entry: "./src/main.js",
-    output: {
-        path: __dirname,
-        filename: "bin/bundle.js"
-    },
-    module: {
-        loaders: [
-            {
-                test: /\.glsl$/,
-                loader: 'webpack-glsl'
-            }
-        ]
-    }
+  "entry": "./src/main.js",
+  "output": {
+    "path": __dirname,
+    "filename": "bin/bundle.js"
+  },
+  "module": {
+    loaders: [
+      {
+        "test": /\.glsl$/,
+        "loader": "webpack-glsl-loader"
+      }
+    ]
+  }
 };
